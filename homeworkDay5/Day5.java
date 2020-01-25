01f709779caa7f493f4ea75129ca73c333c06672
YL-ya <YL_vary@163.com>
2020-01-25 22:41:42 +0800

    homeworkDay5

--- homeworkDay5/Day5.java
+++ homeworkDay5/Day5.java
@@ -2,7 +2,8 @@ package homework;
 import java.util.Arrays;
 public class Day5 {
     /*
-    * 题目1：*/
+    * 题目1：给定一个整数数组，判断是否存在重复元素。如果任何值在数组中重复出现至少两次，函数返回true。
+      如果数组中，每个元素都不同，则返回false。*/
     public static void main(String[] args) {
         int []num={3,1};
         System.out.println(containsDuplicate(num));
@@ -12,7 +13,7 @@ public class Day5 {
         /*for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]+" ");
         }*/
-
+//1：利用两个循环进行判断即可，因为要判断出自身以外的，所以要将本身的那个除外
         for (int i = 0; i <nums.length ; i++) {
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]==nums[j]){
