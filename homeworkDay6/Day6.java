25dad5e9932c6403fd177b052ae7a135b8844852
YL-ya <YL_vary@163.com>
2020-01-29 00:00:01 +0800

    homeworkDay6

--- homeworkDay6/Day6.java
+++ homeworkDay6/Day6.java
@@ -1,11 +1,10 @@
 package homework;
-
 import java.util.Arrays;
-
 public class Day6 {
     /*
     * 题目1：给定一个按非递减顺序排序的整数数组 A，
-    * 返回每个数字的平方组成的新数组，要求也按非递减顺序排序。*/
+    * 返回每个数字的平方组成的新数组，
+     *要求也按非递减顺序排序。*/
     public static void main(String[] args) {
         int []a={1,-3,5,7};
         System.out.println(Arrays.toString(sortedSquares(a)));
