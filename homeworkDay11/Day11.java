05af27f8547e1acca3687630d968d4e9792eb06a
YL-ya <YL_vary@163.com>
2020-02-06 23:19:08 +0800

    homeworkDay11

--- homeworkDay11/Day11.java
+++ homeworkDay11/Day11.java
@@ -1,10 +1,10 @@
 package homework;
-
 public class Day11 {
     public static void main(String[] args) {
         char []chars={'a','a','b','b','b','c','c'};
         System.out.println(compress(chars));
     }
+    //思路：要想数组是在原始数组中进行执行和判断，那么就得用两个指针
     public static int compress(char[] chars) {
         if(chars.length==1){
             return 1;
