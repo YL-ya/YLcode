94b458f58acb1a2cf5357d6c8796549e6236acf8
YL-ya <YL_vary@163.com>
2020-02-16 18:38:44 +0800

    homeworkDay12

--- homeworkDay12/Day12.java
+++ homeworkDay12/Day12.java
@@ -1,7 +1,11 @@
 package homework;
 
+import java.util.Stack;
+
 public class Day12 {
-    public static void main(String[] args) {
+    /*
+    * 题目1：最短无序连续子数组*/
+    public static void main1(String[] args) {
         int[]nums={2,6,4,8,10,9,15};
         System.out.println(findUnsortedSubarray(nums));
     }
@@ -26,5 +30,34 @@ public class Day12 {
             }
         }
         return r-l+1;
+    }
+
+    /*
+    *题目2：逆波兰表达式： */
+    public static void main(String[] args) {
+        String[]a={"2","1","+","3","*"};
+        System.out.println(evalRPN(a));
+    }
+    //思想：定义1个栈，数字存储，然后进行遍历数组，当遇到运算符的时候进行运算即可
+    public static int evalRPN(String[] tokens) {
+        int a = 0, b = 0;
+        Stack<Integer> S = new Stack<Integer>();
+        for (String s : tokens) {
+            if (s.equals("+"))
+                S.add(S.pop() + S.pop());
+            else if (s.equals("-")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a - b);
+            } else if (s.equals("*"))
+                S.add(S.pop() * S.pop());
+            else if (s.equals("/")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a / b);
+            } else
+                S.add(Integer.parseInt(s));
+        }
+        return S.pop();
     }
 }
