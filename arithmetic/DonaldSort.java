90c3a37eae44590b9e8bb0bf65c36d57014f8493
YL-ya <YL_vary@163.com>
2020-02-28 23:24:27 +0800

    DonaldSort

--- arithmetic/DonaldSort.java
+++ arithmetic/DonaldSort.java
@@ -1,6 +1,10 @@
 package com.arithmetic.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+
+import static com.arithmetic.sort.InsertSort.insertSort;
 
 //希尔排序：也相当于一种插入排序，是比插入排序更高效的排序，也成缩小增量排序；
 // 因为当数组为{2,3,4,5,6,1}时，本来只需用交换一次的，但是插入排序需要后移很多次，降低了算法的效率
@@ -10,14 +14,40 @@ import java.util.Arrays;
 * 希尔排序也就是将大的数据尽量往后放*/
 public class DonaldSort {
     public static void main(String[] args) {
-        int[]arr={8,9,1,7,2,3,5,4,6,0};
+        /*int[]arr={8,9,1,7,2,3,5,4,6,0};
         System.out.println("排序前的数组"+Arrays.toString(arr));
-        shellSort(arr);
+        shellSort2(arr);*/
+        //测试事后的时间复杂度：随机产生80000个数
+        int[] arr = new int[80000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 80000);//生成[0,80000）的随机数
+        }
+
+        //将运行的时间进行前后的比较并输出
+        Date date1 = new Date();
+        //将时间进行格式化：
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+        String data1 = simpleDateFormat.format(date1);
+        System.out.println("排序前的时间是：" + data1);
+
+        //进行希尔排序交换法：
+        //shellSort1(arr);
+        /*排序前的时间是：2020-15-28 23:15:40
+        * 排序后的时间是：2020-15-28 23:15:53*/
+
+        //进行希尔排序移动法：
+        shellSort2(arr);
+        /*排序前的时间是：2020-21-28 23:21:34
+        * 排序后的时间是：2020-21-28 23:21:35*/
 
 
+        Date date2=new Date();
+        String data2=simpleDateFormat.format(date2);
+        System.out.println("排序后的时间是："+data2);
     }
     //使用逐步推到的方式，编写希尔排序：在插入的时候使用的是交换法，优化的是移动法
-    public static void shellSort(int[]a){
+    //交换法的希尔排序算法比插入排序的事后时间复杂度还要低，所以进行优化
+    public static void shellSort1(int[]a){
         //根据逐步分析找到规律：使用循环就能解决问题
         int count=0;
         int temp=0;
@@ -33,7 +63,7 @@ public class DonaldSort {
                     }
                 }
             }
-            System.out.println("希尔排序第"+(++temp)+"轮后的数组"+Arrays.toString(a));
+            //System.out.println("希尔排序第"+(++temp)+"轮后的数组"+Arrays.toString(a));
         }
 
 
@@ -78,9 +108,26 @@ public class DonaldSort {
             }
         }
         System.out.println("第三轮排序后的数组："+ Arrays.toString(a));*/
-
-
-
     }
 
+    //希尔排序的交换法的优化：移位法
+    public static void shellSort2(int[]a){
+        //增量的控制采用循环,并逐步缩小增量
+        for (int i = a.length/2; i >0 ; i/=2) {
+            //从i个元素开始，卓哥对其所在的组进行直接插入排序
+            for (int j = i; j < a.length; j++) {
+                int k=j;
+                int temp=a[k];
+                if(a[k]<a[k-i]){//此处有步长
+                    while (k-i>=0&&temp<a[k-i]){
+                        //进行移动：
+                        a[k]=a[k-i];
+                        k-=i;
+                    }
+                    //当退出while循环后就给temp找到了插入的位置
+                    a[k]=temp;
+                }
+            }
+        }
+    }
 }
