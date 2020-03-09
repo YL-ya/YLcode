f0239c41652c3020d2e6051485a500707b416f2b
YL-ya <YL_vary@163.com>
2020-03-10 00:54:27 +0800

    Thread-Study

--- Thread-Study/2/Advantage.java
+++ Thread-Study/2/Advantage.java
@@ -1,5 +1,11 @@
 package lesson2;
-//验证串行和并行的优劣：
+//验证串行和并行的效率：
+/*线程的效率是呈现正态分布，最高效率是和系统资源+线程数+单个线程执行的任务量决定的
+* 影响效率的因素：
+* 1：所有线程执行是并发或者并行的
+* 2：线程的创建和销毁是比较耗时的
+* 3：线程的调度是由系统进行调度的,(线程越多，系统调度的越频繁；线程由就绪态转变成运行态也是会消耗时间的)
+* 4：单个线程的任务量*/
 public class Advantage {
     private static final int NUM=7;
 
@@ -34,6 +40,7 @@ public class Advantage {
         }
         while(Thread.activeCount()>1){//如果活跃线程大于1，就让当前的线程进行等待
             Thread.yield();//注意：在该线程代码进行运行采用debug的方式
+            //yield方法是将当前线程的运行态度转变成就绪态，实现了让不处理
         }
         long end=System.nanoTime();
         System.out.println("并行执行的时间为"+(end-start)/1000/1000);
