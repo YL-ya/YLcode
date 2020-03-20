c88f48bf65f976b363229f2649fdde39f5c0f172
YL-ya <YL_vary@163.com>
2020-03-20 23:54:07 +0800

    MyPriorityQueue

--- dsStructure/MyPriorityQueue.java
+++ dsStructure/MyPriorityQueue.java
@@ -5,13 +5,13 @@ package com.dataStrcture;
 //假设是实现一个小堆
 public class MyPriorityQueue {
     private int[]arr;
-    private int size;//表示优先级队列中有效元素 的个数
+//    private int size;//表示优先级队列中有效元素 的个数
 
     //构造方法1：
     public MyPriorityQueue(){
         //默认的容量大小是11
         arr=new int[11];
-        size=0;
+//        size=0;
     }
 
     //构造方法2：
@@ -21,7 +21,7 @@ public class MyPriorityQueue {
             Capacity=11;
         }
         arr=new int[Capacity];//默认要为11
-        size=0;
+//        size=0;
     }
 
     //构造方法3：
@@ -34,6 +34,12 @@ public class MyPriorityQueue {
             arr[i]=array[i];
         }
         //将arr中的元素进行调整，满足堆的性质
+
+        //找倒数第一个非叶子节点：也就是最后的一个叶子节点(数组中的最后一位)的双亲：(arr.length/2+1）
+        int lastLeaf=arr.length>>1-1;
+        for(int root=lastLeaf;root>=0;root--){
+            shiftDown(root);
+        }
     }
 
     //parent：表示本次需要调整的节点的下标
@@ -48,8 +54,10 @@ public class MyPriorityQueue {
         //标志较小的孩子：child
         //默认情况下先让其标记左孩子，因为parent可能有左孩子，但是没有右孩子
         int child=parent*2+1;
+        int size=arr.length;
 
         while (child<size) {//就是没有孩子节点的时候
+
 
             //在比较左右孩子的时候，一定保证右孩子存在：while()：该循环已经保证左孩子的存在了
             if (child+1<size&&arr[child + 1] < arr[child]) {
@@ -77,5 +85,15 @@ public class MyPriorityQueue {
         int temp=arr[parent];
         arr[parent]=arr[child];
         arr[child]=temp;
+    }
+
+    //测试一下：
+    public static void main(String[] args) {
+        int []arr={5,3,7,1,3,6,8,0,2};
+        MyPriorityQueue mp=new MyPriorityQueue(arr);
+        /*new一个对象：
+        * 1：在堆上分配内存空间
+        * 2：调用构造函数进行初始化*/
+
     }
 }
