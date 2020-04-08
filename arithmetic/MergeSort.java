9ba1b5dc6dee0c5d53fd5c5c5c81f92a2f808535
YL-ya <YL_vary@163.com>
2020-04-08 22:06:24 +0800

    MergeSort

--- arithmetic/MergeSort.java
+++ arithmetic/MergeSort.java
@@ -3,18 +3,25 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
-//归并排序：用的思想是分而治之
+/*归并排序的原理：
+* 1：直接从数组的中间进行划分
+* 2：然后进行归的部分
+* 3：然后进行拷贝*/
+//归并排序的时间复杂度：O(Nlog2N)
+//空间内复杂度：O(log2N+N)==O(log2N):N一般是常量，在长须编译的过程中已经定义好了，所以Nlog2N就是log2N
+//归并排序：用的思想是分而治之：属于外部排序
 /*1·归并排序的时间复杂度是很低的：因为他的复杂度只是数量减一即可
  * 2·归并排序需要额外的空间开销*/
 public class MergeSort {
     public static void main(String[] args) {
-        /*int[]arr={8,4,5,7,1,3,6,2};
+        int[]arr={8,4,5,7,1,3,6,2};
         System.out.println("排序之前的数组："+Arrays.toString(arr));
-        int[]temp=new int[arr.length];
-        mergeSort(arr,0,arr.length-1,temp);
-        System.out.println("排序之后的数组："+Arrays.toString(arr));*/
+        //int[]temp=new int[arr.length];
+        //mergeSort(arr,0,arr.length-1,temp);
+        mergeSortNor(arr);
+        System.out.println("排序之后的数组："+Arrays.toString(arr));
         //测试归并排序的事后时间复杂度：随机产生80000个数
-        int[] arr = new int[80000];
+       /* int[] arr = new int[80000];
         int[]temp=new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 80000);//生成[0,80000）的随机数
@@ -32,7 +39,7 @@ public class MergeSort {
 
         Date date2=new Date();
         String data2=simpleDateFormat.format(date2);
-        System.out.println("排序后的时间是："+data2);
+        System.out.println("排序后的时间是："+data2);*/
     }
     //分+合并
     public static void mergeSort(int[] arr,int left,int right,int[]temp){
@@ -92,4 +99,84 @@ public class MergeSort {
         }
     }
 
+    //优化：没必要一直进行分，直到那个还有16个时候，进行快排
+    private static void mergeSort1(int[]array,int left,int right,int[]temp) {
+        if (right - left > 16) {
+            //insertSort(array, left, right);
+        }else {
+            int mid = left + ((right - left) >> 1);
+            //左半部分:[left,mid)
+            mergeSort1(array, left, mid, temp);
+
+            //右半部分：[mid,right)
+            mergeSort1(array, mid, right, temp);
+
+            //上面是进行分的部分下面进行归的部分：
+            mergeData(array, left, mid, right, temp);
+
+            //归并结束后，有序的数列在temp中
+            //将temp中的数据拷贝到array数组中去
+            //Arrays.copyOf();//这个的底层就是用的System.arraycopy();并且在底层自己申请了空间
+            System.arraycopy(temp,left,array,left,right-left);//是Native方法；并且是浅拷贝
+        }
+    }
+
+    //合并数组称为有序的：[left,mid),[mid,right)
+    private static void mergeData(int[] array,int left,int mid,int right,int[] temp){
+        int index=left;
+        int begin1=left,end1=mid;
+        int begin2=mid,end2=right;
+        while (begin1<end1&&begin2<end2){
+            if(array[begin1]<=array[begin2]){//当时两个数相等的时候，我们先搬1区间中的数字，所以该算法稳定
+                temp[index++]=array[begin1++];
+            }else {
+                temp[index++]=array[begin2++];
+            }
+        }
+
+        //退出循环的话，就是要摸区间1走完了，直接将剩余的区间2中的数字进行搬动即可
+        while (begin1<end1){
+            //1：这样说明的是区间1中还有数据，直接搬动即可
+            temp[index++]=array[begin1++];
+        }
+
+        while (begin2<end2){
+            //2：也就说区间2中海油数据，直接搬动即可
+            temp[index++]=array[begin2++];
+        }
+    }
+
+    //循环的归并：
+    public static void mergeSortNor(int[]array){
+        int[]temp=new int[array.length];
+        int gap=1;
+        while (gap<array.length){
+            for (int i = 0; i <array.length ; i+=gap*2) {
+                int left=i;
+                int mid=left+gap;
+                int right=mid+gap;
+                //保证mid和right不会越界
+                if(mid>array.length){
+                     mid=array.length;
+                }
+                if(right>array.length){
+                    right=array.length;
+                }
+                mergeData(array,left,mid,right,temp);
+            }
+
+            System.arraycopy(temp,0,array,0,array.length);
+            //gap*=2;
+            //gap=gap<<1;
+            gap<<=1;
+        }
+
+
+    }
+
+    //封装：
+    public static void mergeSort1(int[]array){
+        int[]temp=new int[array.length];
+        mergeSort1(array,0, array.length,temp);
+    }
 }
