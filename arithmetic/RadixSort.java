8cfcb4326742fe0724836551ca0150c0c7e52be8
YL-ya <YL_vary@163.com>
2020-04-10 18:56:31 +0800

    RadixSort

--- arithmetic/RadixSort.java
+++ arithmetic/RadixSort.java
@@ -2,9 +2,14 @@ package com.arithmetic.sort;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+//前面的排序方法都是需要进行比较的，但是基数排序不进行比较的
+/*基数排序：鸽巢原理：主要用于数据密集集中在某个范围内
+* 1：统计次数
+* 2：挨个回收*/
 
-//基数排序：属于分配式排序，又称桶排序，它是通过键值的各个位的值，将要排序的元素分配到某些“桶”中，达到排序的作用
-//通俗的来讲，就是一次比较个位，十位，百位，直到比较完最长的数即可停止，也就是排序次数去取决于数组中最大的那个数的长度
+//桶排序：属于分配式排序，又称桶排序，它是通过键值的各个位的值，将要排序的元素分配到某些“桶”中，达到排序的作用
+//通俗的来讲，就是一次比较个位，十位，百位，直到比较完最长的数即可停止，
+// 也就是排序次数去取决于数组中最大的那个数的长度：LSD(低关键码优先：个位，十位，百位)  MSD(高关键码优先：百位，十位，个位)
 /*特点：
 * 1：基数排序是较为稳定的排序，效率也比较的高：也就是说有重复的数字时，前后顺序是不会发生变化的
 * 2：基数排序是桶排序的扩展
@@ -16,8 +21,9 @@ public class RadixSort {
     public static void main(String[] args) {
         /*int[] arr = {53, 3, 542, 748, 14, 214};
         radixSort(arr);*/
+        int[]arr={3,7,2,8,3,1,9,0,4,6,5};
         //测试归并排序的事后时间复杂度：随机产生80000个数
-        int[] arr = new int[80000];
+        /*int[] arr = new int[80000];
         int[]temp=new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 80000);//生成[0,80000）的随机数
@@ -28,17 +34,51 @@ public class RadixSort {
         //将时间进行格式化：
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
         String data1 = simpleDateFormat.format(date1);
-        System.out.println("排序前的时间是：" + data1);
+        System.out.println("排序前的时间是：" + data1);*/
 
         //进行基数排序：
         radixSort(arr);
 
-        Date date2=new Date();
+       /* Date date2=new Date();
         String data2=simpleDateFormat.format(date2);
-        System.out.println("排序后的时间是："+data2);
+        System.out.println("排序后的时间是："+data2);*/
     }
 
-    //基数排序：
+    //基数排序：(比特)：适用场景：数据集中某个范围内
+    /*时间复杂度：O(N)：N 表示区间中元素的个数
+    * 空间复杂度：O(M)：M 表示区间中元素种类的个数
+    * 稳定性：稳定的*/
+    public static void countSort(int[]arr){
+        //1：统计元素范围
+        int minValue=arr[0];
+        int maxValue=arr[0];
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i]>maxValue){
+                maxValue=arr[i];
+            }
+            if(arr[i]<minValue){
+                minValue=arr[i];
+            }
+        }
+        //循环完了只有得出范围：
+        int range=maxValue-minValue+1;
+        int[] arrCount=new int[range];
+
+        //2：统计每个数出现的次数
+        for (int i=0;i<arr.length;i++){
+            arrCount[arr[i]-minValue]++;
+        }
+
+        //3：对元素进行回收：排序
+        int index=0;
+        for (int i = 0; i < range ; i++) {
+            while (arrCount[i]!=0){
+                arr[index]=i+minValue;
+            }
+        }
+    }
+
+    //桶排序：
     public static void radixSort(int[] arr) {
         //根据前面的推导过程，得到最终的基数排序的代码
         /*1：首先得到最大数
