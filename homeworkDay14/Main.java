90ea5a571ec400c675cb263254b96eff11343135
YL-ya <YL_vary@163.com>
2020-04-15 23:22:30 +0800

    Main

--- homeworkDay14/Main.java
+++ homeworkDay14/Main.java
@@ -16,29 +16,28 @@ public class Main {
         sc.close();
     }
     private static String decompose(int n) {
-        int [] array = new int[n] ;
-        int mid = n / 2 ;
-        if(n*n % 2 == 0)
-            array[mid] = n*n + 1;
+        int[] array = new int[n];
+        int mid = n / 2;
+        if (n * n % 2 == 0)
+            array[mid] = n * n + 1;
         else
-            array[mid] = n*n ;
-        for(int i = mid; i >= 1 ; i--){
-            int temp = array[i] ;
-            array[i - 1] = temp - 2 ;
+            array[mid] = n * n;
+        for (int i = mid; i >= 1; i--) {
+            int temp = array[i];
+            array[i - 1] = temp - 2;
         }
-        for(int j = mid ; j < n - 1; j++){
-            int temp = array[j] ;
-            array[j + 1] = temp + 2 ;
+        for (int j = mid; j < n - 1; j++) {
+            int temp = array[j];
+            array[j + 1] = temp + 2;
         }
-        StringBuffer sb = new StringBuffer() ;
-        for(int k = 0 ; k < n ; k++){
-            if(k != n - 1)
-                sb.append(array[k] + "+") ;
+        StringBuffer sb = new StringBuffer();
+        for (int k = 0; k < n; k++) {
+            if (k != n - 1)
+                sb.append(array[k] + "+");
         }
-        sb.append(array[n - 1]) ;
+        sb.append(array[n - 1]);
         return sb.toString();
     }
-
 }
 
 //26：两个超长整数的加法：
