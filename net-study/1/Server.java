938b5e1b4563262f599351bfc7dace403d0be369
YL-ya <YL_vary@163.com>
2020-04-20 00:03:12 +0800

    net-study

--- net-study/1/Server.java
+++ net-study/1/Server.java
@@ -8,44 +8,43 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-//服务器端的IO流操作
-public class Server {
-    private static  final  int PORT=9000;
 
-    //创建一个线程池：
-    private static final ExecutorService POOL=Executors.newCachedThreadPool();
+public class Server {
+
+    private static final int PORT = 9000;
+
+    private static final ExecutorService POOL = Executors.newCachedThreadPool();
 
     public static void main(String[] args) throws IOException {
-        ServerSocket server=new ServerSocket(9000);
-        while (true){
-            //阻塞等待，没有新的客户端进来才会开启线程
-            Socket client=server.accept();
-
-            //当新的客户端的数据流来了，进行处理：采用设计模式，设计一个类
+        ServerSocket server = new ServerSocket(PORT);
+        while (true) {
+            // 阻塞等待，直到有新的客户端连接
+            Socket client = server.accept();
             POOL.execute(new Task(client));
         }
     }
+
     private static class Task implements Runnable{
+
         private Socket client;
 
-        public Task(Socket client){
-            this.client=client;
+        public Task(Socket client) {
+            this.client = client;
         }
 
         @Override
         public void run() {
             try {
-                BufferedReader in=new BufferedReader(new InputStreamReader(client.getInputStream()));
-                PrintWriter out=new PrintWriter(client.getOutputStream(),true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                 String str;
-                int i=1;
-                //阻塞等待，接受数据
-                while ((str=in.readLine())!=null&&!str.equals("exit")){//io流在结束时，返回的才是null
+                //阻塞等待客户端数据输入
+                int i = 1;
+                while((str = in.readLine()) != null){//io流在结束时，返回才是null
                     System.out.println(str);
-                    out.println(i+"确实牛逼");
+                    out.println(i + "确实牛逼");
                     i++;
                 }
-                out.println("exit");
             } catch (IOException e) {
                 e.printStackTrace();
             }
