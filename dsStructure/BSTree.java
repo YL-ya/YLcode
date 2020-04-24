21dd81706fd3cbd42a6a156ba6c2e5751908267a
YL-ya <YL_vary@163.com>
2020-04-24 22:37:11 +0800

    BSTree

--- dsStructure/BSTree.java
+++ dsStructure/BSTree.java
@@ -3,7 +3,13 @@ package com.dataStrcture;
  * 1：中序遍历是有序的
  * 2：最左边的节点是最小的，最右边的节点是最大的
  * 3：二叉搜索树的主要功能是进行搜索的*/
+/*二叉搜索树的查找时间复杂度最差为O(N)：时间复杂度一般看的就是最差的情况下
+* 1：当用户给的数据有序或者接近有序的时候，二叉搜索树退化成单支树，那么时间复杂度会退化成O(N)
+* 2：只有在二叉搜索树为二叉平衡树的时候，时间复杂度是O(log2N):也就是树的高度 */
 
+/*平衡二叉树：
+* 1：AVL树：如果一个树的节点的左右子树高度差值的绝对值不超过1，认为该树是平衡树
+* */
 //binary search tree
 public class BSTree {
     public static class BSTNode{
@@ -176,7 +182,7 @@ public class BSTree {
             while (del.left!=null){//说明不是最左边的节点
                 parent=del;
                 del=del.left;
-            }
+        }
 
             //退出循环说明代替节点已经找到：
             cur.val=del.val;
@@ -192,7 +198,48 @@ public class BSTree {
         return true;
     }
 
-    public static void main(String[] args) {
+    BSTNode prev=null;//标记中序刚刚遍历过的节点
+    //在这里我们进行的是全局变量，可以将值通用带出去，因为将它写成传参的形式，是无法改变实参的值的
+    //二叉搜索树转成有序的双向链表
+    public BSTNode BSTree2Dlist(){
+
+        if(root==null){
+            return null;
+        }
+        //1：找到树的最左侧节点，即双向链表的头
+        BSTNode head=root;
+        while (head.left!=null){
+            head=head.left;
+        }
+
+        //2：修改每个节点的左右指针left和right的指向
+
+        BSTree2Dlist(root);
+        return head;
+    }
+
+    public void BSTree2Dlist( BSTNode root){
+        if(root==null){
+            return;
+        }
+
+        //转化根节点的左子树
+        BSTree2Dlist(root.left);
+
+        //转化根节点：
+        root.left=prev;
+        if(prev!=null){//说明已经标记好了
+            prev.right=root;
+        }
+
+        //用prev将刚刚遍历的节点保存起来
+        prev=root;
+
+        //转化右子树：
+        BSTree2Dlist(root.right);//因为prev如果进行传参的话，那个值是不会改变的
+    }
+
+    public void TestBSTree1(){
         int[]arr={5,3,4,1,7,8,2,6,0,9};
 
         BSTree t=new BSTree();
@@ -209,6 +256,40 @@ public class BSTree {
         t.inOrder();
         t.remove(3);
         t.inOrder();
-        //删除之后中序遍历还是有序的-
+        //删除之后中序遍历还是有序的
+
+    }
+
+    public static void TestBSTree2() {
+        int[] arr = {5, 3, 4, 1, 7, 8, 2, 6, 0, 9};
+
+        BSTree t = new BSTree();
+        for (int e : arr) {
+            t.put(e);
+        }
+        BSTNode head = t.BSTree2Dlist();
+        BSTNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.val +"---->");
+            cur = cur.right;
+        }
+        System.out.println(" null ");
+    }
+
+ /*   public static void TestRefParam1(Integer p){//Integer类是一个final类，是不允许修改的；所以打印出来的都是1
+        p++;
+    }
+    public static void TestRefParam2(Integer p){
+        p=new Integer(100);
+    }*/
+    public static void main(String[] args) {
+        TestBSTree2();
+
+  /*      Integer a=new Integer(1);
+        TestRefParam1(a);
+        System.out.println(a);
+        TestRefParam2(a);
+        System.out.println(a)*/;
+
     }
 }
